d8b30a77b68ff5db4c89f79fcd8cd81dc03b4ed7
Dormin <williamsben91@gmail.com>
2023-10-28 05:29:10 -0600

    test

--- Graphite-Frontend/src/App.js
+++ Graphite-Frontend/src/App.js
@@ -1,59 +1,22 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
-  const audioRef = useRef(null);
-  const canvasRef = useRef(null);
+  const [animation, setAnimation] = useState('');
+
+  const animations = ['bounce', 'slide', 'fade'];
 
   useEffect(() => {
-    const audio = audioRef.current;
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    const analyser = audioContext.createAnalyser();
-
-    audio.addEventListener('play', () => {
-      audioContext.resume().then(() => {
-        const source = audioContext.createMediaElementSource(audio);
-        source.connect(analyser);
-        analyser.connect(audioContext.destination);
-        analyser.fftSize = 256;
-        const bufferLength = analyser.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
-
-        const draw = () => {
-          const WIDTH = canvas.width;
-          const HEIGHT = canvas.height;
-          requestAnimationFrame(draw);
-          analyser.getByteFrequencyData(dataArray);
-
-          ctx.fillStyle = 'rgb(0, 0, 0)';
-          ctx.fillRect(0, 0, WIDTH, HEIGHT);
-
-          const barWidth = (WIDTH / bufferLength) * 2.5;
-          let barHeight;
-          let x = 0;
-
-          for (let i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i];
-            ctx.fillStyle = `rgb(${barHeight + 100},50,50)`;
-            ctx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
-            x += barWidth + 1;
-          }
-        };
-        draw();
-      });
-    });
+    const randomIndex = Math.floor(Math.random() * animations.length);
+    setAnimation(animations[randomIndex]);
   }, []);
 
   return (
-    <div className="App">
-      <canvas ref={canvasRef} width="800" height="400"></canvas>
-      <audio ref={audioRef} controls>
-        <source src="music.mp3" type="audio/mp3" />
-      </audio>
+    <div className={`App ${animation}`}>
+      <h1>Hello, world!</h1>
     </div>
   );
 };
 
 export default App;
+

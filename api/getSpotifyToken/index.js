5ae90f5dc933a507f80c3f70d03293e9a086fb54
Dormin <williamsben91@gmail.com>
2023-10-30 04:02:54 -0600

    added getSpotifyToken

--- api/getSpotifyToken/index.js
+++ api/getSpotifyToken/index.js
@@ -2,18 +2,33 @@ import { post } from 'axios';
 import { stringify } from 'querystring';
 
 export default async function (context, req) {
-  const code = req.query.code;
+  let tokenEndpointData;
 
-  const tokenData = {
-    grant_type: "authorization_code",
-    code: code,
-    redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
-    client_id: process.env.SPOTIFY_CLIENT_ID,
-    client_secret: process.env.SPOTIFY_CLIENT_SECRET
-  };
+  if (req.query.code) {
+    tokenEndpointData = {
+      grant_type: "authorization_code",
+      code: req.query.code,
+      redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      client_secret: process.env.SPOTIFY_CLIENT_SECRET
+    };
+  } else if (req.query.refresh_token) {
+    tokenEndpointData = {
+      grant_type: "refresh_token",
+      refresh_token: req.query.refresh_token,
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      client_secret: process.env.SPOTIFY_CLIENT_SECRET
+    };
+  } else {
+    context.res = {
+      status: 400,
+      body: "Invalid request. Either code or refresh_token is required."
+    };
+    return;
+  }
 
   try {
-    const response = await post('https://accounts.spotify.com/api/token', stringify(tokenData), {
+    const response = await post('https://accounts.spotify.com/api/token', stringify(tokenEndpointData), {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
@@ -29,4 +44,5 @@ export default async function (context, req) {
     };
   }
 };
+
 

1f6c843218d7594b2f9775ca65d81c5de42868cc
Dormin <williamsben91@gmail.com>
2023-11-19 00:59:26 -0700

    added component code for the spotify player

--- Graphite-Frontend/src/SpotifyPlayer.js
+++ Graphite-Frontend/src/SpotifyPlayer.js
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const SpotifyPlayer = ({ token }) => {
-    // Add logic to interact with the Spotify Web Playback SDK
-    // This might include initializing the SDK and handling playback controls
+  // Initialize the Spotify Web Playback SDK
+  useEffect(() => {
+    if (window.Spotify && token) {
+      const player = new window.Spotify.Player({
+        name: 'Your Web Playback SDK Player',
+        getOAuthToken: cb => { cb(token); },
+      });
 
-    return (
-        <div>
-            <button>Play</button>
-            <button>Pause</button>
-            <button>Next</button>
-            <button>Previous</button>
-        </div>
-    );
+      // Ready
+      player.addListener('ready', ({ device_id }) => {
+        console.log('Ready with Device ID', device_id);
+      });
+
+      // Not Ready
+      player.addListener('not_ready', ({ device_id }) => {
+        console.log('Device ID has gone offline', device_id);
+      });
+
+      // Connect to the player
+      player.connect();
+
+      // Disconnect the player on component unmount
+      return () => {
+        player.disconnect();
+      };
+    }
+  }, [token]);
+
+  // Create handlers for the player controls
+  const handlePlay = () => {
+    // Logic to handle play
+  };
+
+  const handlePause = () => {
+    // Logic to handle pause
+  };
+
+  const handleNext = () => {
+    // Logic to handle next track
+  };
+
+  const handlePrevious = () => {
+    // Logic to handle previous track
+  };
+
+  return (
+    <div>
+      <button onClick={handlePlay}>Play</button>
+      <button onClick={handlePause}>Pause</button>
+      <button onClick={handleNext}>Next</button>
+      <button onClick={handlePrevious}>Previous</button>
+    </div>
+  );
 };
 
 export default SpotifyPlayer;
+
